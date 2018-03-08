b6c725aedc0123885fa5681e1b33bcdf08af6c55
Alex Nakagawa <alexnakagawa3@gmail.com>
2018-03-07 18:13:43 -0800

    reformatted main.py and removed unnecesary comments

--- fasta_exercise/alex/main.py
+++ fasta_exercise/alex/main.py
@@ -1,18 +1,12 @@
 
-# coding: utf-8
+'''Week 1 FASTA Exercise'''
 
-# # Week 1 FASTA Exercise
-# 
-# Author: Alex Nakagawa
-# 
-# Last Updated: March 5, 2018
-# 
-# <a src='http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc3'>BioPython Tutorial</a>
-# 
-# <a src='http://biopython.org/DIST/docs/api/'>BioPython Docs</a>
+__author__ = 'Alex Nakagawa'
 
-# In[1]:
+# Last Updated: March 7, 2018
 
+# Tutorial: http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc3
+# Docs: http://biopython.org/DIST/docs/api/
 
 from __future__ import print_function
 
@@ -26,6 +20,7 @@ import pandas as pd
 def main():
 
 	# Reading File
+
 	file_name = '../pdm2_neurogenic.fa'
 
 	print("Reading in Original Sequences:")
@@ -46,42 +41,25 @@ def main():
 	# 	`func`: 0 for negative function, 1 for positive function
 	# 	`GC_ratio`: Ratio of GC pairs to full sequence
 
-	# In[6]:
-
-
 	df_sequences = pd.DataFrame({"id": [i.id for i in sequences],
 	                             "seq": [j.seq for j in sequences],
 	                             "length": [len(k.seq) for k in sequences],
 	                            })
 	df_sequences['func'] = df_sequences['id'].str[-1].replace({'-': 0, '+': 1})
-	df_sequences
-
-
-	# In[39]:
-
-
 	df_sequences['species'] = df_sequences['id'].str.findall('MEMB(....)')
-	df_sequences
 
-
-	# ### Measure GC content per sequence
-
-	# In[20]:
-
+	# Measure GC content per sequence
 
 	df_sequences["GC_ratio"] = df_sequences.seq.apply(GC) / 100.0
-	df_sequences
+	print(df_sequences)
 
-
-	# ### Alignment Sequence
-	# 
-	# `pairwise2.align.globalxx`: Pairwise alignment with no cost value for misaligned pairs, and a value of 1 for matched pairs between the two sequences.
-	# 
-	# `pairwise2.format_alignment(*alignment[0])`: Show matches from start to finish positions as defined by previous function.
-
-	# In[32]:
-
+	# Alignment Sequence
+	# 	`pairwise2.align.globalxx`: Pairwise alignment with no cost value for misaligned pairs, and a value of 1 for matched pairs between the two sequences.
+	# 	`pairwise2.format_alignment(*alignment[0])`: Show matches from start to finish positions as defined by previous function.
 
 	alignment_0_1 = pairwise2.align.globalxx(df_sequences.seq.iloc[0], df_sequences.seq.iloc[1])
 	print(pairwise2.format_alignment(*alignment_0_1[0]))
+
+if __name__ == '__main__':
+	main()
 
